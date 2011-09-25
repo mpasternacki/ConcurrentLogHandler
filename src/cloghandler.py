0111cad1683d5bc3c179022acc7929f4da21451c
Maciej Pasternacki <maciej@pasternacki.net>
2011-09-25 18:17:31 +0200

    Fix error when Django tries to flush a closed file on exit.

--- src/cloghandler.py
+++ src/cloghandler.py
@@ -191,9 +191,10 @@ class ConcurrentRotatingFileHandler(BaseRotatingHandler):
         """ Release file and thread locks. Flush stream and take care of closing
         stream in 'degraded' mode. """
         try:
-            self.stream.flush()
-            if self._rotateFailed:
-                self.stream.close()
+            if not self.stream.closed:
+                self.stream.flush()
+                if self._rotateFailed:
+                    self.stream.close()
         finally:
             try:
                 unlock(self.stream_lock)
